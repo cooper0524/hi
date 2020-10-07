694f6e0c6961b2ac23d14f7c5b8faa12642cd9ab
Cooper Yeh <cooper995601@gmail.com>
2020-10-08 07:08:55 +0800

    Final version

--- guessNum.py
+++ guessNum.py
@@ -2,7 +2,7 @@ import random
 
 player = input("What's your name, Player? ")
 print("Awesome! Let's play a game,", player, "!")
-print("You need to set up a range of numbers later, and guess what Number am I!")
+print("You need to set up a range of numbers, and guess what Number am I!")
 
 while True:
     startNum = input("Please give me a start number: ")
@@ -10,33 +10,35 @@ while True:
     endNum = input("Please give me a end number: ")
     checkEndNum = endNum.isdigit()
     if not checkStartNum or not checkEndNum:
-        print("Make sure you're giving me a proper range of numbers,", player, "!")
+        print("Make sure you're giving me an integer,", player, "!")
     else:
         startNum = int(startNum)
         endNum = int(endNum)
-        target = random.randint(startNum, endNum)
-        print(target)
-        break
-
+        if startNum >= endNum:
+            print("Make sure you're giving me a proper range of numbers,", player, "!")
+        else:
+            target = random.randint(startNum, endNum)
+            break
+        
 print("Great! All set up! Let's begin!!")
-print("I'm between", startNum, "and", endNum, "!")
 
 guessTimes = 0
 while True:
+    print("I'm between", startNum, "and", endNum, "!")
     inputNum = input("Guess what Number am I!! ")
     guessTimes += 1
     checkInputNum = inputNum.isdigit()
     if checkInputNum:
         inputNum = int(inputNum)
         if inputNum == target:
-            print("You Got Me!! Well Done!!! You have tried", guessTimes, "times in total!")
+            print(target, "IT IS!! You Got Me!! Well Done!!! You have tried", guessTimes, "times in total!")
             break
         else:
             if target > inputNum:
                 startNum = inputNum
-                print("Naah! I'm bigger than you thought!! I'm between", inputNum, "and", endNum, "!")
+                print("Naah! I'm bigger than you thought!!")
             else:
                 endNum = inputNum
-                print("Naah! I'm smaller than you thought!! I'm between", startNum, "and", inputNum, "!")
+                print("Naah! I'm smaller than you thought!!")        
     else:
         print("Make sure you're giving me a proper number,", player, "!")
