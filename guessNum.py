0997ba663a0f73fc0b2f21b29d4f2b3ea5893d12
Cooper Yeh <cooper995601@gmail.com>
2020-10-08 06:44:45 +0800

    Let Player decide the range

--- guessNum.py
+++ guessNum.py
@@ -2,9 +2,25 @@ import random
 
 player = input("What's your name, Player? ")
 print("Awesome! Let's play a game,", player, "!")
+print("You need to set up a range of numbers later, and guess what Number am I!")
 
+while True:
+    startNum = input("Please give me a start number: ")
+    checkStartNum = startNum.isdigit()
+    endNum = input("Please give me a end number: ")
+    checkEndNum = endNum.isdigit()
+    if not checkStartNum or not checkEndNum:
+        print("Make sure you're giving me a proper range of numbers,", player, "!")
+    else:
+        startNum = int(startNum)
+        endNum = int(endNum)
+        target = random.randint(startNum, endNum)
+        print(target)
+        break
 
-target = random.randint(1, 100)
+print("Great! All set up! Let's begin!!")
+print("I'm between", startNum, "and", endNum, "!")
+
 guessTimes = 0
 while True:
     inputNum = input("Guess what Number am I!! ")
@@ -18,13 +34,9 @@ while True:
         else:
             if target > inputNum:
                 startNum = inputNum
-                print("Naah! I'm bigger than you thought!!You have tried", guessTimes, "times already!")
+                print("Naah! I'm bigger than you thought!! I'm between", inputNum, "and", endNum, "!")
             else:
                 endNum = inputNum
-                print("Naah! I'm smaller than you thought!!You have tried", guessTimes, "times already!")
+                print("Naah! I'm smaller than you thought!! I'm between", startNum, "and", inputNum, "!")
     else:
         print("Make sure you're giving me a proper number,", player, "!")
-
-
-    
-   
