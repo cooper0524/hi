506b9462fdff85a2a637669b581a116988dfbf61
Cooper Yeh <cooper995601@gmail.com>
2020-10-08 08:10:20 +0800

    Calculat average length of reviews

--- readFile.py
+++ readFile.py
@@ -1,8 +1,15 @@
 data = []
 count = 0
+strLen = 0
 with open("reviews.txt", "r") as f:
     for line in f:
         data.append(line)
         count += 1
         if count % 1000 == 0:
             print(len(data))
+print("File read completed! There are", len(data), "records in total!")
+
+for record in data:
+    strLen += len(record.strip())
+averageLen = strLen/len(data)
+print("The average length of reviews is", averageLen)
