750c649b2e3d75cd6e1cdf5a6d65f12df9a29735
Cooper Yeh <cooper995601@gmail.com>
2020-10-10 19:33:29 +0800

    word count function

--- readFile.py
+++ readFile.py
@@ -1,6 +1,5 @@
 data = []
 count = 0
-strLen = 0
 with open("reviews.txt", "r") as f:
     for line in f:
         data.append(line)
@@ -9,6 +8,8 @@ with open("reviews.txt", "r") as f:
             print(len(data))
 print("File read completed! There are", len(data), "records in total!")
 
+
+strLen = 0
 for record in data:
     strLen += len(record.strip())
 averageLen = strLen/len(data)
@@ -31,3 +32,23 @@ print(len(goodList))
 
 bad = ["bad" in d for d in data]
 print(len(bad))
+
+#Count Words
+wc = {}
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+
+while True:
+    keyWord = input("Search >>> ")
+    if keyWord == "q":
+        break
+    elif keyWord in wc:
+        print(keyWord, "appears", wc[keyWord], "times.")
+    else:
+        print(keyWord, "appears 0 time.")
+print("Thanks for using this searching app!")
